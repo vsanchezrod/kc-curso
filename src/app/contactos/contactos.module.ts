7906f21e0e2405a7f3f408efd89c2c72275f7223
vsanchezrod <v.sanchez.rod@gmail.com>
2018-09-10 10:34:07 +0200

    Creación de formulario en el módulo Contactos

--- src/app/contactos/contactos.module.ts
+++ src/app/contactos/contactos.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContactosRoutingModule } from './contactos-routing.module';
+
+// Componentes
 import { ContactosComponent } from './contactos.component';
+import { FormVdComponent } from './form-vd/form-vd.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ContactosRoutingModule
   ],
-  declarations: [ContactosComponent]
+  declarations: [
+    ContactosComponent,
+    FormVdComponent]
 })
 export class ContactosModule { }
---
+++ src/app/contactos/form-vd/form-vd.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kc-form-vd',
+  templateUrl: './form-vd.component.html',
+  styleUrls: ['./form-vd.component.css']
+})
+export class FormVdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
